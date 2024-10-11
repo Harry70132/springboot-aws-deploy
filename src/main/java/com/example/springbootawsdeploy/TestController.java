607edc781f97bef2223f36323d4674641b240171
Harry70132 <139992973+Harry70132@users.noreply.github.com>
2024-10-11 12:23:44 +0530

    Update TestController.java

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "First message from AWS ECS from HARI--->Version4";
+        return "First message from AWS ECS from HARI--->Version5";
     }
 
     @GetMapping("/message")
